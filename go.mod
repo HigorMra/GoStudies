896bf5f6c9c8d7964ffc4353240d4b99833ce58f
Higor Moreira <moreirahigor007@gmail.com>
2025-10-24 14:50:26 -0300

    Primeiro commit - projeto Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HigorMra/studies-go
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/HigorMra/studies-go/meet"
+)
+
+func main() {
+	meet.SayHello()
+	meet.Say("Olá Higor")
+}
