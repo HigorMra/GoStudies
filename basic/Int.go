2062d08d5bd7d330c3ac1ddf4118be5e6046f40e
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2025-10-29 14:20:02 -0300

    Update Int.go

---
+++ basic/Int.go
@@ -0,0 +1,14 @@
+package basic
+
+import "fmt"
+
+//Utiliza int conforme o numero de bits do sistema ou alterando para usar menos 
+func Int1() {
+	var idade int = 30
+	var contador int32 = 2
+	var indice int8 = 1
+
+	fmt.Println("Idade:", idade)
+	fmt.Println("Idade:", contador)
+	fmt.Println("Idade:", indice)
+}
